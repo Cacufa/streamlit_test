7e2579e07893aad9bbb86666b447c3816f596b9f
Carlos Saborio <carlilloscr@gmail.com>
2022-02-25 13:27:22 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock app price app
+
+Shown are the stock closing price and volume of google!
+
+""")
+
+
+tickerSymbol = 'GOOGL'
+
+tickerData = yf.Ticker(tickerSymbol)
+tickerDF = tickerData.history(period='1d',start='2010-5-31', end='2020-5-31')
+st.line_cart(tickerDF.Close)
+st.line_chart(tickerDF.Volume)
+
